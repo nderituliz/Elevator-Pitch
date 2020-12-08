82c3a17d61c077cabcc4c1c661c3cd5cba34dc91
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-08 14:01:47 +0300

    added route to the registration forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,3 +24,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "Registration"
+    return render_template('auth/register.html',registration_form = form)
+
