751c0813ffbd3a966a41072108688fef37194842
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-08 15:27:53 +0300

    create the route to the home page function

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,17 @@ from ..models import User, PitchCategory, Pitch, Comments, Votes
 # from .forms import UpdateProfile
 from .forms import PitchForm, CommentForm, CategoryForm
 from .. import db
+
+@main.route('/')
+def index():
+    """
+    View root page function that returns index page
+    """
+
+    all_category = PitchCategory.get_categories()
+    all_pitches = Pitch.query.order_by('id').all()
+    print(all_pitches)
+
+    title = 'Home- Welcome'
+    return render_template('index.html', title = title, categories=all_category, all_pitches=all_pitches)
+
