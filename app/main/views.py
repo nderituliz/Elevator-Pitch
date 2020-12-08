264ba5d1da25be1b5a85979c5a405fbc46161514
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-08 15:54:51 +0300

    create a function that allows upvoting and downvoting of piches

--- app/main/views.py
+++ app/main/views.py
@@ -137,6 +137,47 @@ def post_comment(id):
 
     return render_template('post_comment.html', comment_form = form, title = title)
 
+@main.route('/pitch/upvote/<int:id>&<int:vote_type>')
+@login_required
+def upvote(id,vote_type):
+    """
+    View function that adds one to the vote_number column in the votes table
+    """
+    # Query for user
+    votes = Votes.query.filter_by(user_id=current_user.id).all()
+    print(f'The new vote is {votes}')
+    to_str=f'{vote_type}:{current_user.id}:{id}'
+    print(f'The current vote is {to_str}')
+
+    if not votes:
+        new_vote = Votes(vote=vote_type, user_id=current_user.id, pitches_id=id)
+        new_vote.save_vote()
+        print('YOU HAVE new VOTED')
+
+    for vote in votes:
+        if f'{vote}' == to_str:
+            print('YOU CANNOT VOTE MORE THAN ONCE')
+            break
+        else:   
+            new_vote = Votes(vote=vote_type, user_id=current_user.id, pitches_id=id)
+            new_vote.save_vote()
+            print('YOU HAVE VOTED')
+            break
+
+    return redirect(url_for('.view_pitch', id=id))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
