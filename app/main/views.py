9d9c240c141cb3b6e5db794b73b67ea363d004cb
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-08 15:23:56 +0300

    import packages

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
-main = Blueprint('main',__name__)
-from . import views,error
+from . import main
+from flask import render_template,request,redirect,url_for,abort
+from flask_login import login_required,current_user
+from ..models import User, PitchCategory, Pitch, Comments, Votes
+# from .forms import UpdateProfile
+from .forms import PitchForm, CommentForm, CategoryForm
+from .. import db
